7cc5c8e809298bff255765e9d2d195c534118ef3
amr hamdy <amrhamdy15@yahoo.com>
2024-03-28 04:35:14 +0200

    freelancing

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+
+class ListingController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        $search = "%$request->search%";
+        $listings = Listing::whereAny(['title', 'tags', 'company'], 'like', $search)->get();
+        // $tagsString  = $listings->tags;
+        // dd($tagsString);
+
+        // $tagsArray = explode(",", $tagsString);
+
+        if (Auth::user() == null) {
+
+
+            return view('listing', compact('listings'));
+        } else {
+            return view('mange', compact('listings'));
+        }
+    }
+
+    public function create()
+    {
+        return view('createliting');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['required', 'string'],
+        ]);
+        if ($request->has('image')) {
+            $filename = time() . "." . $request->image->extension();
+            $request->logo->move(public_path("images"), $filename);
+            $request->merge([
+                'logo' => $filename
+            ]);
+        }
+        Listing::create($request->all());
+        return redirect()->route('listings.index');
+    }
+
+
+    public function show($id)
+    {
+        $listing = Listing::findOrFail($id);
+        $tagsString  = $listing->tags;
+        $tagsArray = explode(",", $tagsString);
+        return view('showlisting', compact('listing', 'tagsArray'));
+    }
+
+    public function edit($id)
+    {
+        $listing = Listing::findOrFail($id);
+        return view("editlisting", compact("listing"));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $listing = Listing::findorfail($id);
+        if ($request->hasFile('image')) {
+            $filename = time() . '.' . $request->image->extension();
+            $request->image->move(public_path("/images/listings"), $filename);
+            $request->merge([
+                'logo' => $filename
+            ]);
+        }
+        if ($listing->logo !== null && $request->image !== null) {
+            File::delete(public_path("/images/listings/") . $listing->logo);
+        }
+        $listing->update($request->all());
+        return redirect(route('listings.show', $id));
+    }
+
+    public function destroy($id)
+    {
+        $listing = listing::findorfail($id);
+        $listing->destroy($id);
+        return redirect(route('listings.index'));
+    }
+}
---
+++ resources/views/listing.blade.php
@@ -0,0 +1,39 @@
+@extends('layout')
+@section('content')
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
+        @foreach ($listings as $listing)
+            <div class="bg-gray-50 border border-gray-200 rounded p-6">
+                <div class="flex">
+                    <img class="hidden w-48 mr-6 md:block" src="{{ asset("images/listings/$listing->logo") }}"
+                        alt="" />
+                    <div>
+                        <h3 class="text-2xl">
+                            <a href="{{ route('listings.show', $listing->id) }}">{{ $listing->title }}</a>
+                        </h3>
+                        <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
+                        {{-- <x-listing-tags :tagsCsv="$listing->tags" /> --}}
+                        @php
+                            $tags = explode(',', $listing->tags);
+                        @endphp
+                        <ul class="flex">
+                            @foreach ($tags as $tag)
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">{{ $tag }}</a>
+                                </li>
+                            @endforeach
+
+
+                        </ul>
+
+                        <div class="text-lg mt-4">
+                            <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    {{-- {{ $listings->links() }} --}}
+@endsection
